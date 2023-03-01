401e969c309c38a55ad15309cfa09db6d1eecc55
Willian Pinho <kps_wpinho@uolinc.com>
2023-03-01 09:41:51 -0300

    :art: Estilização da tela de Movie

--- MoviesIMDB/MoviesIMDB/Features/Movie/View/MovieViewController.swift
+++ MoviesIMDB/MoviesIMDB/Features/Movie/View/MovieViewController.swift
@@ -11,9 +11,15 @@ class MovieViewController: UIViewController {
     var viewModel: MovieViewModel!
     var movie: Movie?
 
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var crewLabel: UILabel!
+    @IBOutlet weak var rateLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupUI()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -27,6 +33,15 @@ class MovieViewController: UIViewController {
 }
 
 extension MovieViewController {
+    private func setupUI() {
+        guard let currentMovie = movie else { return }
+        posterImageView.load(fromUrl: currentMovie.image ?? "")
+        titleLabel.text = currentMovie.fullTitle
+        crewLabel.text = currentMovie.crew
+        rateLabel.text = "Rating: " + (currentMovie.imDbRating ?? "")
+        ratingLabel.text = "ImdbCounting: " + (currentMovie.imDbRatingCount ?? "")
+    }
+    
     private func setupData() {
         guard let currentMovie = self.movie else { return }
         viewModel.fetchMovie(movie: currentMovie)
